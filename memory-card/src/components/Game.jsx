fbdcf78f8e9f548d86ad366d25169a81df4bd7dd
Vida <quiniavidamarie30@gmail.com>
2026-01-15 23:13:02 +0800

    started styling the cards

--- memory-card/src/components/Game.jsx
+++ memory-card/src/components/Game.jsx
@@ -1,3 +1,5 @@
+import "../styles/Game.css";
+
 import { GetPokemonData } from "./Pokemon";
 
 export function Game({ gameDifficulty }) {
@@ -39,26 +41,30 @@ function GetPokemonDetails({ limit }) {
 function RenderPokemonCard({ pokemonInfoCards }) {
   return (
     <div className="cards">
-      <div className="header">
-        <h4>{pokemonInfoCards.name}</h4>
-        <h4>HP: {pokemonInfoCards.stats[0].base_stat}</h4>
-      </div>
-
-      <div className="card-body">
-        <img src={pokemonInfoCards.sprites.front_default} />
-        <div className="type">
-          <h4>
-            {pokemonInfoCards.types[0].type.name} Pokemon. Length:{" "}
-            {pokemonInfoCards.length} Weight: {pokemonInfoCards.weight}
-          </h4>
+      <div className="cards-info">
+        <div className="header">
+          <h4 className="name">{pokemonInfoCards.name}</h4>
+          <h4>HP: {pokemonInfoCards.stats[0].base_stat}</h4>
         </div>
-        <div className="description">
-          <p>
-            {pokemonInfoCards.speciesInfo.flavor_text_entries[4].flavor_text.replace(
-              /[\f\n\r]/gm,
-              " "
-            )}
-          </p>
+
+        <div className="card-body">
+          <img src={pokemonInfoCards.sprites.front_default} />
+          <div className="type">
+            <h4>{pokemonInfoCards.types[0].type.name} Pokemon</h4>
+
+            <h4>
+              Height: {pokemonInfoCards.height} Weight:{" "}
+              {pokemonInfoCards.weight}
+            </h4>
+          </div>
+          <div className="description">
+            <p>
+              {pokemonInfoCards.speciesInfo.flavor_text_entries[4].flavor_text.replace(
+                /[\f\n\r]/gm,
+                " "
+              )}
+            </p>
+          </div>
         </div>
       </div>
     </div>
