d43273900c71c8d4bdfa9e02cd12559aae9a874a
Vida <quiniavidamarie30@gmail.com>
2026-01-16 15:29:56 +0800

    Finished styling the cards

--- memory-card/src/components/Game.jsx
+++ memory-card/src/components/Game.jsx
@@ -6,6 +6,32 @@ export function Game({ gameDifficulty }) {
   return (
     <main className="main-game">
       <div className="header">
+        <svg style={{ position: "absolute", width: 0, height: 0 }}>
+          <defs>
+            <filter id="pokemonOutline">
+              <feMorphology
+                in="SourceAlpha"
+                result="DILATED"
+                operator="dilate"
+                radius="5"
+              />
+
+              <feFlood floodColor="#2a6eb7" result="BLUE" />
+
+              <feComposite
+                in="BLUE"
+                in2="DILATED"
+                operator="in"
+                result="OUTLINE"
+              />
+
+              <feMerge>
+                <feMergeNode in="OUTLINE" />
+                <feMergeNode in="SourceGraphic" />
+              </feMerge>
+            </filter>
+          </defs>
+        </svg>
         <h1>PokeMatch</h1>
       </div>
 
@@ -41,21 +67,21 @@ function GetPokemonDetails({ limit }) {
 function RenderPokemonCard({ pokemonInfoCards }) {
   return (
     <div className="cards">
-      <div className="cards-info">
-        <div className="header">
+      <div
+        className="cards-info"
+        data-type={pokemonInfoCards.types[0].type.name}
+      >
+        <div className="cards-header">
           <h4 className="name">{pokemonInfoCards.name}</h4>
-          <h4>HP: {pokemonInfoCards.stats[0].base_stat}</h4>
+          <h4>HP:{pokemonInfoCards.stats[0].base_stat}</h4>
         </div>
 
         <div className="card-body">
           <img src={pokemonInfoCards.sprites.front_default} />
           <div className="type">
             <h4>{pokemonInfoCards.types[0].type.name} Pokemon</h4>
-
-            <h4>
-              Height: {pokemonInfoCards.height} Weight:{" "}
-              {pokemonInfoCards.weight}
-            </h4>
+            <h4>Height: {pokemonInfoCards.height}</h4>
+            <h4>Weight: {pokemonInfoCards.weight}</h4>
           </div>
           <div className="description">
             <p>
